ed29c91f438e925639a81df570c1eb4f4470d7cb
Ashh <ashhsmith3@gmail.com>
2025-03-13 02:46:23 +0300

    add

---
+++ add_test.php
@@ -0,0 +1,45 @@
+<?php
+
+include './connect.php';
+
+
+
+$name = filterRequest('name');
+
+$desc = filterRequest('desc');
+
+$price = filterRequest('price');
+
+$discount = filterRequest('discount');
+
+$image1 = filterRequest('image1');
+
+$image2 = filterRequest('image2');
+
+$image3 = filterRequest('image3');
+
+
+
+$data = array(
+
+    'product_name' => $name,
+    'product_price' => $price,
+    'product_image' => $image,
+    // 'product_desc' => $desc,
+
+   
+
+    // 'product_discount' => $discount,
+
+
+    // 'product_image2' => $image2,
+
+    // 'product_image3' => $image3,
+
+    // 'product_image3' => $image3,
+
+    'product_cat' => '15'
+
+);
+
+insertData('products',$data);
