e77ee0bfb8ddf11f33874693cce80ad5e0a69bf7
Ashh-work <ashhsmith3@work.com>
2026-01-23 11:26:08 -0500

    work

--- auth/login.php
+++ auth/login.php
@@ -1,9 +1,65 @@
-<?php
-
-include "../connect.php";
-
-$password = sha1($_POST["password"]);
-$username = filterRequest("username");
 
 
-getData("users", "user_name = ? AND user_password = ?", array($username, $password));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
